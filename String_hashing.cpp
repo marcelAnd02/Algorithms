8e7b298bc448b9a59fe3f646719f6647256fc731
marcelAnd02 <55297735+marcelAnd02@users.noreply.github.com>
2020-06-27 23:42:34 +0200

    Update String_hashing.cpp

--- String_hashing.cpp
+++ String_hashing.cpp
@@ -6,18 +6,18 @@ int MOD7 = 1e9 + 7, BASE = 997;
 const int N = 1e6 + 9;
 int vhash[N], vpow[N];
 
-int rethash(int a, int b)
+int retrieve_hash(int a, int b)
 {
     return (MOD7 + vhash[b] - ((long long)vhash[a - 1] * vpow[b - a + 1]) % MOD7) % MOD7;
 }
 
-void cnt_hash()
+void calc_hash()
 {
     for(int i = 1; i <= a.length(); i++)
         vhash[i] = (((long long)vhash[i - 1] * BASE) % MOD7 + word[i - 1]) % MOD7;
 }
 
-void cnt_pow()
+void calc_pow()
 {
     vpow[0] = 1;
     for(int i = 1; i < N; i++)
@@ -27,9 +27,9 @@ void cnt_pow()
 int main()
 {
     cin >> word;
-    cnt_pow();
-    cnt_hash();
-    //cout << rethash(5, 5) << ' ' << rethash(2, 2);
+    calc_pow();
+    calc_hash();
+    //cout << retrieve_hash(5, 5) << ' ' << retrieve_hash(2, 2);
 
     return 0;
 }
